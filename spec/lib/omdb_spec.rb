4c946b7bf1d126e204ff1f186ebe0f703a4ad761
Jeroen van Baarsen <jeroen@logiconline.nl>
2013-07-11 23:53:32 +0200

    Changed the tests so they dont rely on let and subject so much

--- spec/lib/omdb_spec.rb
+++ spec/lib/omdb_spec.rb
@@ -29,24 +29,50 @@ describe 'Omdb::API' do
 end
 
 describe 'Omdb::Movie' do
-	subject {Omdb::Movie.new(JSON.parse('{"Title":"Star Wars","Year":"1977","imdbID":"tt0076759","Type":"movie"}'))}
 	it "should return an instance of Omdb::Movie when parsed with the correct values" do
-		movie = Omdb::Movie.new(JSON.parse('{"Title":"Star Wars","Year":"1977","imdbID":"tt0076759","Type":"movie"}'))
+		movie = create_movie_object("{}")
 		movie.should be_instance_of(Omdb::Movie)
 	end
 	
 	context "With the basic information loaded" do
-		describe 'the values' do
-			its(:title) {should eq("Star Wars")}
-			its(:year) {should eq(1977)}
-			its(:imdb_id) {should eq('tt0076759')}
-			its(:type) {should eq('movie')}
+		describe '#title' do
+		  it 'returns the title' do
+				movie = create_movie_object('{"Title":"Star Wars"}')
+				movie.title.should eq('Star Wars')
+		  end
 		end
+
+		describe '#year' do
+		  it 'returns the year' do
+				movie = create_movie_object('{"Year":"1977"}')
+				movie.year.should eq(1977)
+		  end
+		end
+
+		describe '#imdb_id' do
+		  it 'returns the IMDB ID' do
+		    movie = create_movie_object('{"imdbID":"tt0076759"}')
+				movie.imdb_id.should eq("tt0076759")
+		  end
+		end
+
+		describe '#type' do
+		  it 'returns the type' do
+		    movie = create_movie_object('{"Type":"movie"}')
+				movie.type.should eq('movie')
+		  end
+		end
+
 	end
 
 	context "Unloaded situation" do
 		it "should have status loaded false" do
-			subject.loaded.should be_false
+			movie = create_movie_object('{}')
+			movie.loaded.should be_false
 		end
+	end
+
+	def create_movie_object(json_data)
+		Omdb::Movie.new(JSON.parse(json_data))
 	end
 end
