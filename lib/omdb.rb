9e9bbf74472e36496b9d4f4c25faa4795e9dceb2
Jeroen van Baarsen <jeroen@logiconline.nl>
2013-07-13 16:47:08 +0200

    Fixed indention (stupid tabs..) & Changed Movie properties to Attr_reader

--- lib/omdb.rb
+++ lib/omdb.rb
@@ -3,36 +3,36 @@ require 'rest_client'
 require 'json'
 
 module Omdb
-	class API
-		def search(search_term)
-			res = RestClient.get 'http://www.omdbapi.com', {params: {s: search_term, r: 'json'}}
-	
-			response = {
-				:movies => parse_movies(res.body),
-				:status => res.code
-			}
-		end
+  class API
+    def search(search_term)
+      res = RestClient.get 'http://www.omdbapi.com', {params: {s: search_term, r: 'json'}}
 
-		private
-		def parse_movies(json_string)
-			data = JSON.parse(json_string)["Search"]
-			movies = Array.new
-			data.each do |movie|
-				movies.push(Movie.new(movie))
-			end
-			return movies
-		end
-	end
+      response = {
+        :movies => parse_movies(res.body),
+        :status => res.code
+      }
+    end
 
-	class Movie
-		attr_accessor :loaded, :title, :year, :rated, :released, :runtime, :genre, :director, :writer, :actors, :plot,
-									:poster, :imdb_rating, :imdb_votes, :imdb_id, :type
-		def initialize(movie)
-			@loaded = false # Default the details are not loaded
-			@title = movie["Title"]	
-			@year = movie["Year"].to_i
-			@imdb_id = movie["imdbID"]
-			@type = movie["Type"]
-		end
-	end
+    private
+    def parse_movies(json_string)
+      data = JSON.parse(json_string)["Search"]
+      movies = Array.new
+      data.each do |movie|
+        movies.push(Movie.new(movie))
+      end
+      return movies
+    end
+  end
+
+  class Movie
+    attr_reader :loaded, :title, :year, :rated, :released, :runtime, :genre, :director, :writer, :actors, :plot,
+                :poster, :imdb_rating, :imdb_votes, :imdb_id, :type
+    def initialize(movie)
+      @loaded = false # Default the details are not loaded
+      @title = movie["Title"]
+      @year = movie["Year"].to_i
+      @imdb_id = movie["imdbID"]
+      @type = movie["Type"]
+    end
+  end
 end
--- spec/lib/omdb_spec.rb
+++ spec/lib/omdb_spec.rb
@@ -1,78 +1,78 @@
 require 'omdb'
 
 describe 'Omdb::API' do
-	subject {Omdb::API.new}
-	let(:omdb_api) {Omdb::API.new}
-	it "Should return a instance of OMDB::API" do
-		omdb_api.should be_instance_of(Omdb::API)
-	end
+  subject {Omdb::API.new}
+  let(:omdb_api) {Omdb::API.new}
+  it "Should return a instance of OMDB::API" do
+    omdb_api.should be_instance_of(Omdb::API)
+  end
 
-	context "When searching for 'Star Wars'" do
-		let(:response) {subject.search("Star Wars")}	
-	
-		it "responds to a search call" do
-			omdb_api.should respond_to(:search).with(1).argument
-		end
+  context "When searching for 'Star Wars'" do
+    let(:response) {subject.search("Star Wars")}
 
-		it "returnr status code 200" do
-			response[:status].should eq(200)		
-		end
+    it "responds to a search call" do
+      omdb_api.should respond_to(:search).with(1).argument
+    end
 
-		it "returns a total of 10 items" do
-			response[:movies].size.should eq(10)	
-		end
+    it "returnr status code 200" do
+      response[:status].should eq(200)
+    end
 
-		it "returned list movies should be of the type Omdb::Movie" do
-			response[:movies][0].should be_instance_of(Omdb::Movie)
-		end
-	end
+    it "returns a total of 10 items" do
+      response[:movies].size.should eq(10)
+    end
+
+    it "returned list movies should be of the type Omdb::Movie" do
+      response[:movies][0].should be_instance_of(Omdb::Movie)
+    end
+  end
 end
 
 describe 'Omdb::Movie' do
-	it "should return an instance of Omdb::Movie when parsed with the correct values" do
-		movie = create_movie_object("{}")
-		movie.should be_instance_of(Omdb::Movie)
-	end
-	
-	context "With the basic information loaded" do
-		describe '#title' do
-		  it 'returns the title' do
-				movie = create_movie_object('{"Title":"Star Wars"}')
-				movie.title.should eq('Star Wars')
-		  end
-		end
+  it "should return an instance of Omdb::Movie when parsed with the correct values" do
+    movie = create_movie_object("{}")
+    movie.should be_instance_of(Omdb::Movie)
+  end
 
-		describe '#year' do
-		  it 'returns the year' do
-				movie = create_movie_object('{"Year":"1977"}')
-				movie.year.should eq(1977)
-		  end
-		end
+  context "With the basic information loaded" do
+    describe '#title' do
+      it 'returns the title' do
+        movie = create_movie_object('{"Title":"Star Wars"}')
+        movie.title.should eq('Star Wars')
+      end
+    end
 
-		describe '#imdb_id' do
-		  it 'returns the IMDB ID' do
-		    movie = create_movie_object('{"imdbID":"tt0076759"}')
-				movie.imdb_id.should eq("tt0076759")
-		  end
-		end
+    describe '#year' do
+      it 'returns the year' do
+        movie = create_movie_object('{"Year":"1977"}')
+        movie.year.should eq(1977)
+      end
+    end
 
-		describe '#type' do
-		  it 'returns the type' do
-		    movie = create_movie_object('{"Type":"movie"}')
-				movie.type.should eq('movie')
-		  end
-		end
+    describe '#imdb_id' do
+      it 'returns the IMDB ID' do
+        movie = create_movie_object('{"imdbID":"tt0076759"}')
+        movie.imdb_id.should eq("tt0076759")
+      end
+    end
 
-	end
+    describe '#type' do
+      it 'returns the type' do
+        movie = create_movie_object('{"Type":"movie"}')
+        movie.type.should eq('movie')
+      end
+    end
 
-	context "Unloaded situation" do
-		it "should have status loaded false" do
-			movie = create_movie_object('{}')
-			movie.loaded.should be_false
-		end
-	end
+  end
 
-	def create_movie_object(json_data)
-		Omdb::Movie.new(JSON.parse(json_data))
-	end
+  context "Unloaded situation" do
+    it "should have status loaded false" do
+      movie = create_movie_object('{}')
+      movie.loaded.should be_false
+    end
+  end
+
+  def create_movie_object(json_data)
+    Omdb::Movie.new(JSON.parse(json_data))
+  end
 end
