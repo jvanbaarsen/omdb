3ef78ec2aa9408040d04c503653a420bcf6610ff
Nicholas Palaniuk <npalaniuk@luxre.com>
2015-02-06 13:37:50 -0800

    Remove redundant stuff

--- lib/omdb/api.rb
+++ lib/omdb/api.rb
@@ -3,49 +3,37 @@ require 'omdb/movie'
 module Omdb
   class Api
     def search(search_term)
-      res = network.call({s: search_term})
+      res = network.call(s: search_term)
       if res[:data]["Response"] == "False"
-        response = {
-          movies: {},
-          status: 404
-        }
+        { movies: {}, status: 404 }
       else
-        response = {
-          movies: parse_movies(res[:data]),
-          status: res[:code]
-        }
+        { movies: parse_movies(res[:data]), status: res[:code] }
       end
     end
-    
+
     # fetchs a movie with a given title
     # set tomatoes to true if you want to get the rotten tomatoes ranking
     # set plot to full if you want to have the full, long plot
     def fetch(title, year = "", tomatoes = false, plot = "short")
-      res = network.call({t: title, y: year, tomatoes: tomatoes, plot: plot})
-      
+      res = network.call({ t: title, y: year, tomatoes: tomatoes, plot: plot })
+
       if res[:data]["Response"] == "False"
-        response = {status: 404}
+        { status: 404 }
       else
-        response = {
-          status: res[:code],
-          movie: parse_movie(res[:data])
-        }
+        { status: res[:code], movie: parse_movie(res[:data]) }
       end
     end
-    
+
     # fetches a movie by IMDB id
     # set tomatoes to true if you want to get the rotten tomatoes ranking
     # set plot to full if you want to have the full, long plot
     def find(id, tomatoes = false, plot = "short")
-      res = network.call({i: id, tomatoes: tomatoes, plot: plot})
-      
+      res = network.call({ i: id, tomatoes: tomatoes, plot: plot })
+
       if res[:data]["Response"] == "False"
-        response = {status: 404}
+        { status: 404 }
       else
-        response = {
-          status: res[:code],
-          movie: parse_movie(res[:data])
-        }
+        { status: res[:code], movie: parse_movie(res[:data]) }
       end
     end
 
@@ -56,7 +44,7 @@ module Omdb
       data.each do |movie|
         movies.push(parse_movie(movie))
       end
-      return movies
+      movies
     end
 
     def parse_movie(data)
--- lib/omdb/network.rb
+++ lib/omdb/network.rb
@@ -4,7 +4,7 @@ module Omdb
   class Network
     OMDB_API_URL = 'http://www.omdbapi.com'
     def call(params)
-      response = RestClient.get OMDB_API_URL, {params: params}
+      response = RestClient.get OMDB_API_URL, params: params
       {
         code: response.code,
         data: JSON.parse(response.body)
